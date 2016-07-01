48d00c7c22b8ab5b2627c69a51f9d0480667a760
Eric Brand <eric.d.brand@gmail.com>
2016-07-01 16:02:19 -0500

    Updated tests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello JENKINS!');
+	res.send('Hello');
 });
 
 app.listen(process.env.PORT || 5000);
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-	it('respond with Hello world!', function(done) {
-		request(app).get('/').expect('Hello JENKINS!', done);
+	it('respond with "Hello"', function(done) {
+		request(app).get('/').expect('Hello', done);
 	});
 });
