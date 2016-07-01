e3c94351bb9d880c19856f4e6d3d468b2528837f
Eric Brand <eric.d.brand@gmail.com>
2016-07-01 15:39:47 -0500

    Updated tests

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
 	it('respond with Hello world!', function(done) {
-		request(app).get('/').expect('Hello world!', done);
+		request(app).get('/').expect('Hello JENKINS!', done);
 	});
 });
