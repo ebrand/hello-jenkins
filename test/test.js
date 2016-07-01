b25d79e45adea43307c0781a415e2a96ae408f1b
Eric Brand <eric.d.brand@gmail.com>
2016-07-01 13:58:54 -0500

    Add node app

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+	it('respond with Hello world!', function(done) {
+		request(app).get('/').expect('Hello world!', done);
+	});
+});
